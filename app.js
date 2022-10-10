5b37209db5d5c44dd96f996cf34b2c1d55e81318
Cecy Flores <anacecyflores1@gmail.com>
2022-10-10 17:11:47 -0500

    Challenge #2 Completed: Odd & Even Numbers

---
+++ app.js
@@ -0,0 +1,16 @@
+// practice 1
+// function reverseString(str) {
+//   var splitString = str.split("");
+//   return splitString;
+// }
+
+// reverseString("hello");
+
+// practice 2- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(n) {
+  if (n % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
