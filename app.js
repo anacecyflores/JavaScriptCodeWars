dc846c76b61124dcc78f2d76f32694b042def497
Cecy Flores <anacecyflores1@gmail.com>
2022-11-20 20:02:23 -0600

    Rock, paper Scissors

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-let num = 15;
+const rps = (p1, p2) => {
+  if (p1 === "rock" && p2 === "paper") {
+    return "Player 2 won!";
+  }
 
-function numberToString(num) {
-  let text = num.toString();
-  return text;
-}
+  if (p1 === "paper" && p2 === "rock") {
+    return "Player 1 won!";
+  }
 
-console.log(numberToString(num));
+  if (p1 === "rock" && p2 === "scissors") {
+    return "Player 1 won!";
+  }
+
+  if (p1 === "scissors" && p2 === "rock") {
+    return "Player 2 won!";
+  }
+
+  if (p1 === "scissors" && p2 === "paper") {
+    return "Player 1 won!";
+  }
+  if (p1 === "paper" && p2 === "scissors") {
+    return "Player 2 won!";
+  }
+  if (p1 === p2) {
+    return "Draw!";
+  }
+};
