ccb8cd306d809825812c2b78859e2171a0244789
Cecy Flores <anacecyflores1@gmail.com>
2022-11-20 21:17:24 -0600

    Remove 1st and Last Character of a string

---
+++ removeChar.js
@@ -0,0 +1,21 @@
+// function removeChar(str) {
+//   //You got this!
+// }
+// var Str = "Hello";
+
+// // removing character 'e'
+// var newStr = Str.substring(0, -1);
+
+// console.log(newStr);
+
+const text = "hello";
+function word(text) {
+  const editFirst = text.slice(1).slice(0, -1);
+  return editFirst;
+
+  console.log(word(text));
+}
+//   const editLast = text.slice(0, -1);
+//   let finalWord = editFirst + editLast;
+
+// const finalWord = editFirst + editLast;
