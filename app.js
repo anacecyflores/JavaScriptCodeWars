f2cbc8f707c18e50e136cc9f165dc1d835a04205
Cecy Flores <anacecyflores1@gmail.com>
2022-11-28 21:24:24 -0600

    Completed Remove Spaces Challenge with Regex

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let str = "how you doin Doug";
+let str = "how you doin";
 
 function noSpace(str) {
-  let newOutcome = str.replaceAll(" ", "");
+  let newOutcome = str.replace(/ /g, "");
   return newOutcome;
 }
 console.log(noSpace(str));
