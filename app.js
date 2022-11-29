20db86fd16291e447ad2e1637570312fda5ba75c
Cecy Flores <anacecyflores1@gmail.com>
2022-11-28 21:34:35 -0600

    Completed doubling integers in an array

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-let str = "how you doin";
 
-function noSpace(str) {
-  let newOutcome = str.replace(/ /g, "");
-  return newOutcome;
-}
-console.log(noSpace(str));
--- removeSpace.js
+++ removeSpace.js
@@ -5,3 +5,12 @@ function noSpace(str) {
   return newOutcome;
 }
 console.log(noSpace(str));
+
+// // WITH REGEX FORM:
+// let str = "how you doin";
+
+// function noSpace(str) {
+//   let newOutcome = str.replace(/ /g, "");
+//   return newOutcome;
+// }
+// console.log(noSpace(str));
