7777a39c8669097c6e3a155c743ffb4130d28571
Cecy Flores <anacecyflores1@gmail.com>
2022-12-14 20:06:22 -0600

    Convert years into centuries: 1800-19th century

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-// let arrA = [1, 2, 3];
-// let arrB = [1, 1, 1];
-// function arrayPlusArray(arrA, arrB) {
-//   return arrA.concat.arrB;
-// }
-// console.log(arrayPlusArray(arrA, arrB));
-// function sum_array(arrA, arrB) {
-//   // store our final answer
-//   var sum = 0;
-
-//   //   loop through entire array
-//   for (var i = 0; i < arrA.length; i++) {
-//     // loop through each inner array
-//     for (var j = 0; j < arrB.length; j++) {
-//       // add this number to the current final sum
-//       sum += arr[i][j];
-//     }
-//   }
-
-//   return sum;
+// function centuryFromYear(year) {
+//   if (typeof year == "string")
+//     if (year.toString().slice(-2) == "00") return year.toString().slice(0, 2);
+//     else return (Math.floor(+year / 100) + 1).toString();
+//   else if (typeof year == "number") return Math.floor((year - 1) / 100) + 1;
+//   else return undefined;
 // }
 
-// sum_array([[3, 2], [1], [4, 12]]);
+// // console.log(centuryFromYear("2000"));
+// // console.log(centuryFromYear("1901"));
+// // console.log(centuryFromYear("2002"));
+// console.log(centuryFromYear(2002));
+// console.log(centuryFromYear(1999));
+// console.log(centuryFromYear(2000));
 
-let arr1 = [1, 2, 3, 1];
-let arr2 = [2, 3, 4, 1];
-
-function arrayPlusArray(arr1, arr2) {
-  let joinedArr = arr1.concat(arr2);
-  // joinedArr = [1, 2, 3, 1, 2, 3, 4]
-  let sum = 0;
-
-  for (let i = 0; i < joinedArr.length; i++) {
-    sum = sum + joinedArr[i];
-  }
-  return sum;
+// Solution:
+function century(year) {
+  if (typeof year == "string")
+    if (year.toString().slice(-2) == "00") return year.toString().slice(0, 2);
+    else return (Math.floor(+year / 100) + 1).toString();
+  else if (typeof year == "number") return Math.floor((year - 1) / 100) + 1;
+  else return undefined;
 }
-
-console.log(arrayPlusArray(arr1, arr2));
+console.log(century("2000"));
+console.log(century("1901"));
+console.log(century("2002"));
+console.log(century(2002));
+console.log(century(1999));
+console.log(century(2000));
