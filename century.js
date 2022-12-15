8c2d66e2bdc68ddf4f251e99ce7c11a6c0c195a6
Cecy Flores <anacecyflores1@gmail.com>
2022-12-14 22:27:56 -0600

    Years of pet ownership: Cats & Dog Years

--- century.js
+++ century.js
@@ -9,3 +9,29 @@
 // 1900 --> 19
 // 1601 --> 17
 // 2000 --> 20
+
+// Solution:
+function century(year) {
+  // if it's a string
+  if (typeof year == "string") {
+    if (year.toString().slice(-2) == "00") {
+      return year.toString().slice(0, 2);
+    } else {
+      return (Math.floor(+year / 100) + 1).toString();
+    }
+  }
+  // if it's a number
+  if (typeof year == "number") {
+    return Math.floor((year - 1) / 100) + 1;
+  }
+  // otherwise
+  else {
+    return undefined;
+  }
+}
+console.log(century("2000"));
+console.log(century("1901"));
+console.log(century("2002"));
+console.log(century(2002));
+console.log(century(1999));
+console.log(century(2000));
