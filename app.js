e4f3bd8ab9ba8d13c5ca9f2e2217fd13a5ca41ab
Cecy Flores <anacecyflores1@gmail.com>
2022-12-22 17:50:38 -0600

    Need to finish the football points solution

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-function points(games) {
-  return 0;
-}
+
--- footballPoints.js
+++ footballPoints.js
@@ -19,3 +19,17 @@
 // 0 <= y <= 4
 // ___________________________________________
 // SOLUTION:
+
+// function points(games) {
+//   let result = [];
+//   let score = 0;
+//   games.forEach((x) => {
+//     result = x.split(":");
+//     if (parseInt(result[0]) > parseInt(result[1])) score += 3;
+//     else if (result[0] == result[1]) score += 1;
+//     else score += 0;
+//   });
+//   return score;
+// }
+
+// NOT DONE!!!!!!!!!!!!!!
