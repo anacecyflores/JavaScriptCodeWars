5eaaea492f3e5d2e6ff023d60559cf7b20ad73c7
Cecy Flores <anacecyflores1@gmail.com>
2022-12-22 18:05:53 -0600

    updated capitalization error

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const word = "chicago";
 
+const lastLetter = word.charAt(word.length - 1);
+const secondLetter = word.charAt(word.length - 2);
+const thirdLetter = word.charAt(word.length - 3);
+
+const lastThreeLetters = [thirdLetter, secondLetter, lastLetter];
+
+console.log(lastThreeLetters);
--- lastThreeLetters.js
+++ lastThreeLetters.js
@@ -2,8 +2,8 @@ const word = "chicago";
 
 const lastLetter = word.charAt(word.length - 1);
 const secondLetter = word.charAt(word.length - 2);
-const ThirdLetter = word.charAt(word.length - 3);
+const thirdLetter = word.charAt(word.length - 3);
 
-const lastThreeLetters = [ThirdLetter, secondLetter, lastLetter];
+const lastThreeLetters = [thirdLetter, secondLetter, lastLetter];
 
 console.log(lastThreeLetters);
