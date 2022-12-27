ab4e6de2d62324ca2c28caabe1838cec86ecd8e9
Cecy Flores <anacecyflores1@gmail.com>
2022-12-27 15:14:25 -0600

    true needed to be posted as true withouth the  marks

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ let bullets = 5;
 let dragons = 20;
 
 function hero(bullets, dragons) {
-  if (bullets !== dragons * 2) return "true";
-  if (bullets > dragons * 2) return "true";
-  if (dragons > bullets * 2) return "false";
+  if (bullets >= dragons * 2) return true;
+  //   if (bullets > dragons * 2) return "true";
+  //   if (dragons > bullets * 2) return "false";
   else {
     bullets === dragons;
-    return "false";
+    return false;
   }
 }
 console.log("hero", bullets, dragons, hero(bullets, dragons));
