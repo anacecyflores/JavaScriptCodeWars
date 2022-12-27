f6fd9687958017097c2d69656e436be777b3dafd
Cecy Flores <anacecyflores1@gmail.com>
2022-12-27 16:19:53 -0600

    Final Solution for basic math

--- basicMath.js
+++ basicMath.js
@@ -8,3 +8,35 @@
 // ('-', 15, 18) --> -3
 // ('*', 5, 5) --> 25
 // ('/', 49, 7) --> 7
+
+// // let m = (*)
+// // let a = (+)
+// // let s = (-)
+// // let d = (/)
+
+// let value1 = ''
+// let value2 = ''
+// let operationA = "+"
+// let operationS = "-"
+// let operationD = "/"
+// let operationM = "*"
+
+// function basicOp(operation, value1, value2) {
+//  if operationA => { value1 + value2}
+//  } return basicOp()
+//  if operationS => { value1 - value2 }
+// if operationD => { value1 / value2 }
+// if operationM => { value1 * value2}
+
+// console.log (basicOp("+", 1, 2).tostring())
+
+// SOLUTION_______________________________________
+function basicOp(operation, value1, value2) {
+  if (operation === "-") return value1 - value2;
+  if (operation === "/") return value1 / value2;
+  if (operation === "+") return value1 + value2;
+  if (operation === "*") return value1 * value2;
+  else return "not an operation";
+}
+
+console.log(basicOp("*", 5, 5));
