9afb488dcbbc97e07efc6a1191284b7cbfc38e1a
Cecy Flores <anacecyflores1@gmail.com>
2022-12-27 18:06:29 -0600

    New Challenge completed: is N divisible by X and Y?

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+let x = 2;
+let y = 6;
 
+function isDivisible(n, x, y) {
+  if (n % x == 0 && n % y == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isDivisible(12, x, y));
