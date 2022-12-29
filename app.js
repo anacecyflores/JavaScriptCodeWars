2b10c45a09dcd8f5eda6d8450e29de7d3bfcbda8
Cecy Flores <anacecyflores1@gmail.com>
2022-12-29 13:37:47 -0600

    Attempted BMI Cal, only one test passed.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let x = 2;
-let y = 6;
+// let underweight = 18.5;
+// let normal = 25.0;
+// let overweight = 30.0;
+// let obese = 30.1;
 
-function isDivisible(n, x, y) {
-  if (n % x == 0 && n % y == 0) {
-    return true;
-  } else {
-    return false;
-  }
+let weight = 29;
+let height = 2;
+
+function bmi(weight, height) {
+  if ((weight * height) / 2 <= 18.5 > 25) return "Underweight";
+  if ((weight * height) / 2 <= 25 < 18.5) return "Normal";
+  if ((weight * height) / 2 <= 30 > 25) return "Overweight";
+  if ((weight * height) / 2 >= 30) return "Obese";
 }
-console.log(isDivisible(12, x, y));
+
+console.log(bmi(weight, height));
