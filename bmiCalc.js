f18bcb6a05eb5570e22981805b51d0077ea4c769
Cecy Flores <anacecyflores1@gmail.com>
2022-12-29 21:22:35 -0600

    Finished Calc BMI)

--- bmiCalc.js
+++ bmiCalc.js
@@ -7,3 +7,31 @@
 // if bmi <= 30.0 return "Overweight"
 
 // if bmi > 30 return "Obese"
+if (weight / Math.pow(height, 2) <= 18.5) {
+  return "Underweight";
+}
+if (weight / Math.pow(height, 2) <= 25) {
+  return "Normal";
+}
+if (weight / Math.pow(height, 2) <= 30) {
+  return "Overweight";
+}
+if (weight / Math.pow(height, 2) > 30) {
+  return "Obese";
+}
+
+// let underweight = 18.5;
+// let normal = 25.0;
+// let overweight = 30.0;
+// let obese = 30.1;
+
+console.log(bmi(weight, height));
+
+function bmi(weight, height) {
+  if ((weight * height) / 2 <= 18.5 > 25) return "Underweight";
+  if ((weight * height) / 2 <= 25) return "Normal";
+  if ((weight * height) / 2 <= 30 < 25) return "Overweight";
+  if ((weight * height) / 2 >= 30) return "Obese";
+}
+let weight = 70;
+let height = 250;
