ef4eda7fb16566f17fc9a98fc442cc1478d6d005
Cecy Flores <anacecyflores1@gmail.com>
2023-01-29 22:14:11 -0600

    Convert positive to negative and vice versa

---
+++ oppositeNumber.js
@@ -0,0 +1,27 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return number * -1;
+}
+
+// NOT THE SOLUTION:_____________
+// let number = 10;
+// function opposite(number) {
+//   if (number === 0) {
+//     return "zero";
+//   }
+//   if (number < 0);
+//   {
+//     return (number = Math.abs(number));
+//   }
+//   if (number > 0) {
+//     return -number;
+//   }
+// }
+// console.log(opposite(number));
