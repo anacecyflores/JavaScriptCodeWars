e7fe1dcad63b83ef0998c61cca5a208dae297ec3
Cecy Flores <anacecyflores1@gmail.com>
2023-02-02 22:18:13 -0600

    If name starts with R or r they play the Banjo '

--- oppositesAttract.js
+++ oppositesAttract.js
@@ -4,3 +4,15 @@
 // function lovefunc(flower1, flower2){
 //   // moment of truth
 // }
+let flower1 = 6;
+let flower2 = 6;
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 == 0 && flower2 % 2 == 0) ||
+    (flower2 % 2 == 1 && flower1 % 2 == 1)
+  ) {
+    return false;
+  } else return true;
+}
+
+console.log(lovefunc(flower1, flower2));
