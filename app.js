0a6b6e94811bbcc83670d6b0d235dafd7d31e09e
Cecy Flores <anacecyflores1@gmail.com>
2023-02-15 23:08:35 -0600

    needle in a haystack kata using.length method

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let haystack = ["hi", "bye", "haystack", "needle"];
 
+function findNeedle(haystack) {
+  // your code here
+  for (i = 0; i < haystack.length; i++) {
+    if (haystack[i] === "needle") {
+      return "found the needle at position " + i;
+    }
+  }
+}
+
+console.log(findNeedle(haystack));
