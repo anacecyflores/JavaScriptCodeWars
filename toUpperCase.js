4acca1f8255bd938e69bf77787a8d7138bb9f7d0
Cecy Flores <anacecyflores1@gmail.com>
2023-06-21 11:41:12 -0500

    ToUpperCase challenge done

---
+++ toUpperCase.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+let str = "hello";
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+// console.log(str.toUpperCase());
+console.log(makeUpperCase(str));
