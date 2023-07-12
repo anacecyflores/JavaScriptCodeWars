c12a44ce621abcdddb64cbfbaf86011ba14d2e78
Cecy Flores <anacecyflores1@gmail.com>
2023-07-12 09:22:52 -0500

    No zeros for heroes

---
+++ previous-challenges/noZeros.js
@@ -0,0 +1,26 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+let n = 1450;
+
+function noBoringZeros(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  while (n % 10 === 0) {
+    n = n / 10;
+  }
+
+  return n;
+}
+
+console.log(noBoringZeros(n));
