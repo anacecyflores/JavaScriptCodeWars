655c842413bf5b8566b90e8fa3e36fd4f7179d90
Cecy Flores <anacecyflores1@gmail.com>
2023-09-14 17:11:46 -0500

    Code Under Pressure

---
+++ codeUnderPressure.js
@@ -0,0 +1,10 @@
+// function doubleInteger(i) {
+//   i * 2;
+//   return doubleInteger(i);
+// }
+function doubleInteger(i) {
+  var total = i * 2;
+  return total;
+}
+
+console.log(doubleInteger(5));
